7f30ead45ce71198fa1a47e62c9bc7e5b4ed1695
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-09-08 15:53:57 +0100

    att

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Iuri 'Borja'"); // String = Texto
+console.log('Iuri "Borja"');
+console.log(`'Iuri' "Borja"`);
+
+console.log(12345); // Number = Número
+
+console.log(40, 17.03, 'Iuri Borja');
